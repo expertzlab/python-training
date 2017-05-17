5d17a2e645ab3eb3b32a4c9438822225ea8f6e39
Gireesh Babu <gireesh.babu@nibodha.com>
2017-05-17 16:47:21 +0530

    intial set of files conditions and while loop

---
+++ first.py
@@ -0,0 +1,26 @@
+def findGreater(a, b):
+    if(a > b):
+        return a
+    elif(a != b):
+        return b
+    else:
+        return "They are equal"
+        
+            
+
+result = findGreater(101, 191)
+print ("Result = ", result )
+result = findGreater(100 , 450)
+print ("Result = ", result )
+
+
+
+
+    
+    
+    
+    
+    
+
+
+
